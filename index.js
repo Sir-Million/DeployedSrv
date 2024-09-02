1a2d1ba588bc24df56205d33e06f4167f468c818
Sir_Milllion <73513663+Sir-Million@users.noreply.github.com>
2024-09-02 01:14:04 -0600

    .

--- index.js
+++ index.js
@@ -4,20 +4,18 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Servir archivos estáticos desde la carpeta 'public'
+// Configuración para servir archivos estáticos desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Ruta principal que sirve 'index.html'
+// Ruta principal
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'main.html'));
 });
 
-// Manejo de rutas no encontradas (404)
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
 
-// Iniciar el servidor
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
 });
